f358a3c1cb540594c2182f4b4a80b6fab991d722
Anthony-Villamar <138512747+Anthony-Villamar@users.noreply.github.com>
2025-07-06 19:52:08 -0500

    Update logicaventa.js

--- Cliente-fronted/logicaventa.js
+++ Cliente-fronted/logicaventa.js
@@ -13,7 +13,7 @@ async function cargarProductos() {
     const div = document.createElement("div");
     div.className = "producto";
     div.innerHTML = `
-      <img src="${p.imagen_url}" alt="Imagen de ${p.nombre}" class="img-fluid">
+      <img src="${p.imagen_url}" alt="Imagen de ${p.nombre}" class="img-fluid" style="max-height:150px; object-fit:contain;">
       <h4>${p.nombre}</h4>
       <div class="descripcion">
         <p>${p.descripcion}</p>
