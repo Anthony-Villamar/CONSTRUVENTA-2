f36f135661f7eaab6beb5d3c0c6752a0b16bc239
Anthony-Villamar <138512747+Anthony-Villamar@users.noreply.github.com>
2025-07-19 23:41:01 -0500

    Update logicaventa.js1234

--- Cliente-fronted/logicaventa.js
+++ Cliente-fronted/logicaventa.js
@@ -239,10 +239,7 @@ const totalFinal = subtotal + subtotalFinal;
         body: JSON.stringify({ id_pedido_global, transporte_precio: totalTransporte })
         // body: JSON.stringify({ id_pedido_global, transporte_precio: totalTransporte })
 
-      }).then(res => {
-            if (!res.ok) throw new Error("❌ Error en /factura");
-            return res.json();
-          });
+      });
   
       // ✅ 3. Preparar envio promise si hay transporte seleccionado
       // En logicaventa.js ➔ dentro de onApprove
